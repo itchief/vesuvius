d6a000b83e11ed58983a1731ea08337cd0e6cf92
itchief <alex.malcev1980@gmail.com>
2024-08-07 15:34:00 +1000

    initial skeleton web project

---
+++ app/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'db' => [
+        'database' => 'app',
+        'user' => 'root',
+        'password' => 'root',
+        'host' => 'mysql',
+        'port' => '3306',
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'options' => [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
+        ]
+    ],
+];
---
+++ html/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require __DIR__ . '/../app/vendor/autoload.php';
+$config = require __DIR__ . '/../app/config.php';
+
+echo 'Hello, world!';
