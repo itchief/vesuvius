13fbd8269a211c35ae4119ab854eb9fdb4deb846
itchief <alex.malcev1980@gmail.com>
2024-08-08 15:25:07 +1000

    Update

--- html/index.php
+++ html/index.php
@@ -3,4 +3,29 @@
 require __DIR__ . '/../app/vendor/autoload.php';
 $config = require __DIR__ . '/../app/config.php';
 
-echo 'Hello, world!';
+use App\Core\Connection;
+
+try {
+    $connection = new Connection($config['db']);
+    $statement = $connection->run('TRUNCATE users');
+
+    $id = $connection->insertAndGetId('INSERT INTO users (name, email, created_at, updated_at) VALUES (:name, :email, :created_at, :updated_at)', [
+        'name' => 'Leo',
+        'email' => 'leo@mail.com',
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_at' => date('Y-m-d H:i:s')
+    ]);
+
+    $row = $connection->selectOne('SELECT * FROM users WHERE id = :id', [
+        'id' => $id
+    ]);
+
+    if (is_array($row)) {
+        echo '<pre>';
+        print_r($row);
+        echo '</pre>';
+    }
+
+} catch (PDOException $e) {
+    echo $e;
+}
